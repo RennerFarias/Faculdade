5f24539acefbc2e50fd3c6a5e9ea7d7cf5631df1
Renner <rennerbarbosadefarias76@gmail.com>
2025-03-05 20:32:32 -0300

    Update atividade1.py

--- Estudos Python abella/terceira lista/atividade1.py
+++ Estudos Python abella/terceira lista/atividade1.py
@@ -1,1 +1,8 @@
- 
+frutas = ["maçã", "banana", "laranja", "uva", "manga"]
+for i in frutas :
+    print(i)
+
+    #toda vez que um item da lista é impresso, o loop é executado novamente, até que todos os itens sejam impressos.
+    #O loop for é usado para iterar sobre uma sequência de itens. ~
+
+    #sistema de locadora para alugar filmes dia 27/09/2025
