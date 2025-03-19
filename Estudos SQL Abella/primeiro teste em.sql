dcdb7e0c05e07e9b2f6fc65ad7bc9eb7e2294497
Renner <rennerbarbosadefarias76@gmail.com>
2025-03-19 20:57:19 -0300

    Primeiro codigo em sql

---
+++ Estudos SQL Abella/primeiro teste em.sql
@@ -0,0 +1,17 @@
+/*create table conta (
+	conta int,
+    agencia int,
+    titular varchar(100),
+    saldo float,
+	primary key (conta)
+*/
+
+/*
+insert into conta (conta,agencia,titular,saldo)
+values (1234,1,'renner',2500);
+*/
+
+/*
+update conta set saldo = 5232 where conta = 1234;
+*/
+select * from conta;
